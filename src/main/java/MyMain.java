9ade8369b8407485fbe82f7e5e621548b34bf596
22asingh <22asingh@athenian.org>
2020-09-27 23:49:21 +0000

    defined methods and tested in main

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,20 +5,29 @@ public class MyMain {
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+        return (int)((Math.random()*6)+1);
     }
 
     // Rolls two dice n times
     // This method returns an int array of size 11, where each element is
     // the number of times a certain sum of two dice was rolled
     public static int[] sumOfTwoDice(int n) {
-        // REPLACE YOUR CODE HERE
-        return null;
+        int[] sumTotals = new int[11];
+        for (int i = 0; i < n; i++){
+            sumTotals[(rollDie() + rollDie())-2] += 1;
+        }
+        return sumTotals;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many pair of dice would you like to roll?");
+        int rollNum = scan.nextInt();
+        int[] sumTotals = sumOfTwoDice(rollNum);
+        for (int i = 0; i < sumTotals.length; i++){
+            System.out.println("% of rolls where sum == " + (i + 2) + ": " + (sumTotals[i]*100.0)/rollNum);
+        }
+        scan.close();
     }
 }
